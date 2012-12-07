6704f9ae9990cebde2f26e03550d3318d50e08aa
jenniferdr <jenniferdosreis@hotmail.com>
2012-12-07 12:13:28 -0430

    LISTOO

--- weirdPrimes2.cpp
+++ weirdPrimes2.cpp
@@ -47,9 +47,11 @@ int main(int argc,char** argv){
 
   for(int i=0; i<N ; i++){
     stream >> firstHalves[i];
+    //printf("Primera mitad: %s \n",firstHalves[i].c_str());
   }
   for(int i=0; i<N; i++){
     stream >> secondHalves[i];
+    //printf("Primera mitad: %s \n",secondHalves[i].c_str());
   }
 
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
@@ -57,17 +59,24 @@ int main(int argc,char** argv){
   
   // Cantidad de primos que le corresponde a cada proc
   int base= N/numProc;
-  int resto= N%numProc;
+  //int resto= N%numProc;
   
-  list<int> primos;
+  long long int primos[base+1];
+  int indice=0;
  
   // Cada proceso verifica los numeros q le toca
   for(int i=myid*base; i<myid*base+base ; i++){
     for(int j=0; j<N ; j++){
       string strNumero= firstHalves[i];
       strNumero.append(secondHalves[j]);
-      int numero= atoi(strNumero.c_str());
-      if(esPrimo(numero)) primos.push_back(numero); 
+      printf("El string del primo: %s \n",strNumero.c_str());
+      long long int numero= atoi(strNumero.c_str());
+      cout << "Viendo si es primo: "<< numero << "\n";
+      if(esPrimo(numero)) {
+	primos[indice]=numero;
+	//printf("%d \n",numero);
+	indice++;
+      }
     }
   }
 
@@ -77,19 +86,42 @@ int main(int argc,char** argv){
       for(int j=0; j<N ; j++){
 	string strNumero= firstHalves[i];
 	strNumero.append(secondHalves[j]);
-	int numero= atoi(strNumero.c_str());
-	if(esPrimo(numero)) primos.push_back(numero); 
+	long long int numero= atoi(strNumero.c_str());
+	if(esPrimo(numero)){
+	  primos[indice]=numero;
+	  //printf("%d \n ",numero);
+	  indice++;
+	}
       }
     }
   }
-  
-  
-
-  if(myid==0){
-    // Recibir de todos 
-    // ordenar
-    // Imprimir en orden 
+  for(int i=indice; i<(base+1); i++){
+    primos[i]=-1;
   }
+  
+  int *buff;
+  if(myid==0){
+    buff= (int*) malloc(sizeof(long long int)*numProc*(base+1));
+  }
+
+  MPI_Gather(primos,base+1,MPI_INT,buff,base+1,MPI_INT,0,MPI_COMM_WORLD);
+
+  list<long long int> listprimos;
+  if(myid==0){
+    for(int i=0; i<((base+1)*numProc); i++){
+      if(buff[i]!=-1){
+	listprimos.push_back(buff[i]);
+      }
+    }
+
+    listprimos.sort();
+    list<long long int>::iterator i;
+
+    for(i=listprimos.begin(); i!=listprimos.end();i++){
+      cout << *i << "\n";
+    }
+  }
+  cout << sizeof(long long int);
 
   MPI_Finalize();
   return 0;
